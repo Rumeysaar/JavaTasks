a021ee4ce4bcf8beee2f9b9039e3ab38793f9241
Rumeysa <rumeysaarsvn@gmail.com>
2023-06-15 14:41:29 +0200

    ch08 While Loop tasks

---
+++ src/Tasks08_whileLoops/Task01.java
@@ -0,0 +1,33 @@
+package Tasks08_whileLoops;
+
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+/*
+Task-> girilen sayıların toplamı 333 gectiğinde girilen sayı adedini ve toplamını prşnt eden code create ediniz.
+ */
+
+        Scanner input = new Scanner(System.in);
+
+
+
+
+        int toplam = 0;
+        int count = 0;
+
+        while (toplam <= 333) {
+            System.out.println("bir tam sayi giriniz");
+            int sayi = input.nextInt();
+            toplam += sayi;
+            count ++;
+        }
+        System.out.println("girilen sayi adedi " + count + "\n" + "toplam " + toplam);
+
+
+
+
+
+
+    }
+}
---
+++ src/Tasks08_whileLoops/Task02.java
@@ -0,0 +1,35 @@
+package ch08_Loops.L02_WhileLoop.Tasks08_whileLoops;
+
+import java.util.Scanner;
+
+public class Task02 {
+    public static void main(String[] args) {
+        /*
+        task-> girilen iki tamsayı arasındaki tamsayıları print eden code create ediniz.
+        for loop & while lopp
+         */
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("bir tam sayi giriniz");
+        int sayi1 = input.nextInt();
+        System.out.println("bir tam sayi giriniz");
+        int sayi2 = input.nextInt();
+
+       if (sayi2 > sayi1) {
+           for (int i = sayi1; i <= sayi2; i++) {
+               System.out.println(i);
+           }
+       }
+       else if (sayi1 > sayi2) {
+           for (int i = sayi2; i <= sayi1; i++) {
+               System.out.println(i);
+           }
+       }
+
+
+
+
+
+
+    }
+}
